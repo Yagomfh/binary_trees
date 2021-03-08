a8e5de7acd4605ccf9d3868e407f93db99590634
Battsetseg20 <tsetseg.yo@gmail.com>
2021-03-08 10:57:47 +0000

    feat: initial solution

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - Deletes an entire binary tree
+ * tree - pointer to the root node
+ * if tree is NULL, do nothing
+ **/
+
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (!tree)
+		return;
+
+	binary_tree_delete(tree->left);
+	binary_tree_delete(tree->right);
+	free(tree);
+	tree = NULL;
+}
