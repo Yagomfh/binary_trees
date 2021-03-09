53ba04cc623a8c1d590940b9545ae2de2ece1b4e
Battsetseg20 <tsetseg.yo@gmail.com>
2021-03-09 14:54:40 +0000

    feat: advanced 28

---
+++ 114-bst_remove.c
@@ -0,0 +1,66 @@
+#include "binary_trees.h"
+bst_t *minValueNode(bst_t *tree);
+/**
+ * bst_remove - removes a node from a Binary Search Tree
+ * @root: pointer to the root node of the tree where you will remove a node
+ * @value:value to remove in the tree
+ * Return: pointer to the new root node of the tree after removal of value
+ * Node containing a value equals to value must be removed and freed
+ * Node with two children:replace with its 1st in-order successor
+ **/
+
+bst_t *bst_remove(bst_t *root, int value)
+{
+	bst_t *tmp;
+
+	if (!root)
+		return (root);
+	else if (value < root->n)
+		/* search in the left */
+		root->left = bst_remove(root->left, value);
+	else if (value > root->n)
+		/* search in the right */
+		root->right = bst_remove(root->right, value);
+
+	else /* when value = root->n */
+	{
+		/* no child or 1 child */
+		if (root->left == NULL)
+		{
+			tmp = root;
+			root = root->right;
+			free(tmp);
+		}
+		else if (root->right == NULL)
+		{
+			tmp = root;
+			root = root->left;
+			free(tmp);
+		}
+		/* 2 children */
+		/* Inorder successor: smallest element in right subtree */
+		else
+		{
+			tmp = minValueNode(root->right);
+			root->n = tmp->n;
+			root->right = bst_remove(root->right, tmp->n);
+		}
+	}
+	return (root);
+}
+
+/**
+ * minValueNode - finds the smallest element in BST subtree
+ * @root: pointer to the roor if the tree
+ * Return: smallest element
+ */
+
+bst_t *minValueNode(bst_t *root)
+{
+
+	while (root->left != NULL)
+	{
+		root = root->left;
+	}
+	return (root);
+}
